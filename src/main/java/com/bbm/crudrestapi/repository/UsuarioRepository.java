fcc45522e891b532cfe1b9fa02311d641f7f070c
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-08-26 13:10:52 +0200

    adicionando a query de pesquisa por nome

--- src/main/java/com/bbm/crudrestapi/repository/UsuarioRepository.java
+++ src/main/java/com/bbm/crudrestapi/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.bbm.crudrestapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bbm.crudrestapi.model.UsuarioEntity;
@@ -8,4 +11,6 @@ import com.bbm.crudrestapi.model.UsuarioEntity;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
+	@Query(value = "select u from UsuarioEntity u where upper(trim(u.nomecompleto)) like %?1%")
+	public List<UsuarioEntity> findByName(String name);
 }
